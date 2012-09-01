78529dcfb13e3f225ce4a72c4dbbdf4b8ae4c73f
Mathieu Agopian <mathieu.agopian@gmail.com>
2012-09-01 16:48:15 +0200

    fix templates, again

--- tcp/core/urls.py
+++ tcp/core/urls.py
@@ -6,6 +6,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="home.html"),
+    url(r'^$', TemplateView.as_view(template_name="core/home.html"),
         name='home'),
 )
